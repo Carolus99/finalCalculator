780e9c4a00e2c87546a796e2728bfa1319d5cbf9
Chase <chasecomfort@gmail.com>
2020-08-29 14:24:46 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//DO NOT USE EVAL() FUNCTION
+
+function add (){
+
+
+}
+
+function subtract (){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
+
+function operate(operator, number1, number2){
+
+}
+
+
